7b4677d74b41192854e2b46d1f14f29a78dcf13e
Tharindu Jayakody <140710028+atssamadhi@users.noreply.github.com>
2023-07-30 22:28:59 +0530

    Add files via upload

---
+++ PHP/tutorial11/tute11_1.php
@@ -0,0 +1,15 @@
+<?php
+// Task 1
+$x = 6;
+$y = 4;
+
+$sum = $x + $y;
+$difference = $x - $y;
+$product = $x * $y;
+$division = $x / $y;
+
+echo "Sum: " . $sum . "\n";
+echo "Difference: " . $difference . "\n";
+echo "Product: " . $product . "\n";
+echo "Division: " . $division . "\n";
+?>
