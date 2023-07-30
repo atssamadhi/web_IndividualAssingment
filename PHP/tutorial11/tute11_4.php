b55b72ae14178b2f80157f8190f723a985aaf55c
Tharindu Jayakody <140710028+atssamadhi@users.noreply.github.com>
2023-07-30 22:36:00 +0530

    Add files via upload

---
+++ PHP/tutorial11/tute11_4.php
@@ -0,0 +1,31 @@
+<?php
+// Task 4
+$dayNumber = 3; // Replace this number with the desired day number
+
+switch ($dayNumber) {
+  case 1:
+    echo "Monday";
+    break;
+  case 2:
+    echo "Tuesday";
+    break;
+  case 3:
+    echo "Wednesday";
+    break;
+  case 4:
+    echo "Thursday";
+    break;
+  case 5:
+    echo "Friday";
+    break;
+  case 6:
+    echo "Saturday";
+    break;
+  case 7:
+    echo "Sunday";
+    break;
+  default:
+    echo "Invalid number";
+}
+?>
+
